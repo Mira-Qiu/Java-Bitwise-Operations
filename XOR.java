4b7b9b600c337b4bdda5291ee98435c6a44563af
Mira Qiu <qmm0607@gmail.com>
2019-05-20 08:47:27 -0400

    Create XOR.java

---
+++ XOR.java
@@ -0,0 +1,17 @@
+/*
+Bitwise XOR(^): it returns bit by bit XOR of input value, i.e, if corresponding bits are different, it gives 1, else it gives 0.
+*/
+
+public class Test {
+      public static void main(String[] args){
+            System.out.println(5 ^ 3);  //  output : 6
+      }
+}
+
+
+/**
+5 = 0101
+3 = 0011
+________
+    0110
+**/
